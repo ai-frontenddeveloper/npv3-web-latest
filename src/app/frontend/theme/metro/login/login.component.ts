932bbb505d27947bb53712823017cdea0920bbc5
Asim Iqbal <asim.frontenddeveloper@gmail.com>
2018-11-13 16:06:07 +0500

    remove responsive component

--- src/app/frontend/theme/metro/login/login.component.ts
+++ src/app/frontend/theme/metro/login/login.component.ts
@@ -5,7 +5,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss', './login.responsive.component.scss']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
